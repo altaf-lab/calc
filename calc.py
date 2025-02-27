17cf81b356f96be655dca99e87457fea8b766953
altaf-lab <altafaipa95@gmail.com>
2025-02-27 14:22:17 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,29 +1,27 @@
 import streamlit as st
 
+def calculator(num1, num2, opr):
+    if opr == "+":
+        return num1 + num2
+    elif opr == "-":
+        return num1 - num2
+    elif opr == "*":
+        return num1 * num2
+    else:
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Division by zero is not allowed"
 
-    def calculator(num1,num2,opr):
-  
-      if opr=="+":
-          return num1+num2
-      elif opr=="-":
-          return num1-num2
-      elif opr=="*":
-          return num1*num2
-      else:
-          if num2!=0:
-              return num1/num2
-          else:
-              return "Division by zero is not allowed"
-  
-  def main():   
-      st.title("welcome to My Calculator...")    
-      number1=st.number_input("Enter number..",format="%2f")
-      operator=st.selectbox("Select operator",["+","-","*","/"])
-      number2=st.number_input("Enter Number..",format="%2f")
-  
-      result=calculator(number1,number2,operator)
-      if st.button("calculate"):
-          st.success(result)
-  
-  if __name__=="__main__":
-      main()
+def main():
+    st.title("Welcome to My Calculator...")
+    number1 = st.number_input("Enter number 1:", format="%f")
+    operator = st.selectbox("Select operator", ["+", "-", "*", "/"])
+    number2 = st.number_input("Enter number 2:", format="%f")
+
+    if st.button("Calculate"):
+        result = calculator(number1, number2, operator)
+        st.success(result)
+
+if __name__ == "__main__":
+    main()
